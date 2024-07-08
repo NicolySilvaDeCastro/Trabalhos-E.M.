a5380914a89d17c150527c4a0a14fba1154a5b48
Nicoly Silva de Castro <129177560+NicolySilvaDeCastro@users.noreply.github.com>
2024-07-08 09:50:19 -0300

    commit bd

---
+++ aula01.sql
@@ -0,0 +1,25 @@
+-- alter table cliente 
+-- add column endereco varchar(150)
+
+-- drop table cliente
+-- CREATE TABLE cliente(
+-- id serial primary key,
+-- nome varchar(100),
+-- email varchar(50),
+-- telefone varchar(11),
+-- cpf varchar (11)
+-- );
+
+-- insert into cliente(nome, email, telefone, cpf) 
+-- values('PEDRO', 'pedro@uol.com', '48992454878', '98765432125')
+
+-- select * from cliente
+
+-- CREATE TABLE senha(
+-- id_senha serial primary key,
+-- id_user int,
+-- senha varchar(15),
+-- foreign key(id_user) references usuario(id_user)
+-- );
+-- drop table senha
+-- select * from usuario
